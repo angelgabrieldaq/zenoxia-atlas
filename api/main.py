1c859100d4fa5dee29fa89cb1cf63080c7301d89
Angel Acuña <angel.acua@gmail.com>
2026-06-09 17:20:27 -0300

    http://localhost: a allow origins *

--- api/main.py
+++ api/main.py
@@ -24,7 +24,7 @@ app = FastAPI(
 # CORS: permite el front local durante desarrollo.
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
